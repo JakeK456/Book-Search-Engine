bc24c2f6cc8dcbd0593f87b66203672a097617b7
JK <jrileykruger@gmail.com>
2022-06-05 13:21:23 -0700

    me query saved books

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -2,7 +2,35 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Query {
-    ping: String
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    description: String!
+    bookId: String!
+    title: String!
+    authors: [String]
+    image: String
+    link: String
   }
 `;
 
